f96a2de280815718315739ac029c55778dcc61de
Olga <kartoshkaslukom@gmail.com>
2024-11-02 20:13:51 +0100

    feat: add profiles endpoint

--- src/routes/graphql/types.ts
+++ src/routes/graphql/types.ts
@@ -1,4 +1,4 @@
-import { GraphQLObjectType, GraphQLString, GraphQLFloat, GraphQLInt } from 'graphql';
+import { GraphQLObjectType, GraphQLString, GraphQLFloat, GraphQLInt, GraphQLBoolean, GraphQLList } from 'graphql';
 
 export const MemberType = new GraphQLObjectType({
     name: 'MemberType',
@@ -16,4 +16,27 @@ export const MemberType = new GraphQLObjectType({
       title: { type: GraphQLString },
       content: { type: GraphQLString },
     },
+  });
+
+  export const ProfileType = new GraphQLObjectType({
+    name: 'ProfileType',
+    fields: {
+    id: { type: GraphQLString },
+    isMale: { type: GraphQLBoolean },
+    yearOfBirth: { type: GraphQLInt },
+    memberType: {type: MemberType}
+},
+});
+
+  export const UserType = new GraphQLObjectType({
+    name: 'UserType',
+    fields: () => ({
+            id: { type: GraphQLString },
+            name: { type: GraphQLString },
+            balance: { type: GraphQLFloat },
+            profile: { type: ProfileType },
+            posts: {type: new GraphQLList(PostType)},
+            userSubscribedTo: {type: new GraphQLList(UserType)},
+            subscribedToUser: {type: new GraphQLList(UserType)}
+    }),
   });
